00db03523824fbcabcbd4663f1bbefd4ff8d3f23
GAPrado <guilhermeprado@outlook.com>
2023-11-02 22:22:29 -0300

    add ecs agent

--- user_data/launch_template.sh
+++ user_data/launch_template.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Install ECS agent
 yum update -y
+
+# Install ECS agent
 yum install -y amazon-ecs-agent
 
 # Start ECS agent
 start ecs
+
+# Install SSM agent
+yum install -y amazon-ssm-agent
+
+# Start SSM agent
+start amazon-ssm-agent
