d3dee38464de29b3bf6e592225cd3009cbd4f9ea
GAPrado <guilhermeprado@outlook.com>
2023-11-03 12:28:54 -0300

    add ecs agent

--- user_data/launch_template.sh
+++ user_data/launch_template.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-echo ECS_CLUSTER=mikes_cluster >> /etc/ecs/ecs.config
-
 sudo yum -y update
 
+# Install and configure the CloudWatch Logs agent
+sudo yum install -y awslogs
+sudo service awslogs start
+sudo chkconfig awslogs on
+
+# ecs agent
 sudo amazon-linux-extras disable docker
+sudo amazon-linux-extras install -y ecs
+echo ECS_CLUSTER=mikes_cluster >> /etc/ecs/ecs.config
+sudo systemctl enable --now ecs
 
-sudo amazon-linux-extras install -y ecs; sudo systemctl enable --now ecs
-
+# ssm agent
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-
 sudo systemctl start amazon-ssm-agent
