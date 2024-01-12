59e20a0db448f04b6d40b329bb22f2775cfc9bb1
wagnerlm <wagnerlink@gmail.com>
2024-01-12 20:15:30 -0300

    redis

--- main.tf
+++ main.tf
@@ -278,6 +278,6 @@ resource "aws_elasticache_cluster" "redis" {
   engine               = "redis"
   node_type            = "cache.m4.large"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis"
+  parameter_group_name = "default.redis3.2"
   port                 = 6379
 }
