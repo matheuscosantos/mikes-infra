0be0d6709dfe415a19feec3742df51b616d49d05
wagnerlm <wagnerlink@gmail.com>
2024-01-12 20:13:20 -0300

    redis

--- main.tf
+++ main.tf
@@ -271,3 +271,13 @@ resource "aws_lb_listener" "lb_listener" {
     target_group_arn = aws_lb_target_group.lb_target_group.arn
   }
 }
+
+
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "order-cache"
+  engine               = "redis"
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis"
+  port                 = 6379
+}
